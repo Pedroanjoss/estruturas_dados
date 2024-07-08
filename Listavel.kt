84a606fc9525e8b99b81ff973f86f8b50066275d
Pedro dos Anjos <128007522+Pedroanjoss@users.noreply.github.com>
2024-07-08 15:53:56 -0300

    Create Listavel.kt

---
+++ Listavel.kt
@@ -0,0 +1,15 @@
+interface Listavel {
+    fun anexar(dado: Any?)
+    fun inserir(posicao:Int, dado: Any?)
+    fun selecionar(posicao: Int)
+    fun selecionarTodos(): Array<Any>
+    fun atualizar(posicao: Int, dado: Any?)
+    fun apagar(posicao: Int): Any?
+    fun apagarTodos(): Array<Any?>
+    fun limpar()
+
+    fun tamanho(): Int
+    fun estaCheia(): Boolean
+    fun estaVazia(): Boolean
+    fun imprimir(): String
+}
